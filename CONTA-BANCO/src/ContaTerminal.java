13a708214f1c4a60819d9fc072f47d8d4bfdc7a9
jaelson <jaelson.jaelsonsantos.freitas@gmail.com>
2024-05-13 16:58:10 -0300

    Criação de uma conta bancaria

--- CONTA-BANCO/src/ContaTerminal.java
+++ CONTA-BANCO/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Olá Mundo!");
-    }
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("por favor, digite o número da conta:");
+        int número = scanner.nextInt();
+
+        System.out.println("por favor, digite o número da agência:");
+        scanner.nextLine();
+        String agencia = scanner.nextLine();
+
+
+        System.out.println("por favor, digite o nome do cliente:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("por favor, digite o saldo:");
+        double saldo = scanner.nextDouble();
+
+        
+        String mensagem = "Olá" + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " ja está disponível para saque.";
+            System.out.println(mensagem);
+
 }
+}
+  
